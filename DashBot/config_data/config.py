112c84b986e3edba7d59930514eacfb42b4595be
Alexey Inkov <122154128+AlexeyInkov@users.noreply.github.com>
2023-11-28 19:40:51 +0300

    Update config.py

--- DashBot/config_data/config.py
+++ DashBot/config_data/config.py
@@ -5,10 +5,10 @@ from telebot.types import BotCommand
 
 from servises.bot_states import UserInfoState
 
-if not find_dotenv():
-    exit("Переменные окружения не загружены т.к отсутствует файл .env")
-else:
-    load_dotenv()
+# if not find_dotenv():
+#     exit("Переменные окружения не загружены т.к отсутствует файл .env")
+# else:
+#     load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = os.getenv("ADMIN_ID")
