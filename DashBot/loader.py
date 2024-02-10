0d8990515961c99b1f6243afb2177ba7e2edbcb4
Alexey Inkov <alex0982@mail.ru>
2024-02-10 17:09:46 +0300

    fix structura(mkdir)

--- DashBot/loader.py
+++ DashBot/loader.py
@@ -1,3 +1,5 @@
+import os
+
 from telebot import TeleBot
 from telebot import StateMemoryStorage
 from loguru import logger
@@ -7,5 +9,7 @@ from config_data import config
 
 storage = StateMemoryStorage()
 bot = TeleBot(token=config.BOT_TOKEN, state_storage=storage)
+if not os.path.exists('photos'):
+    os.mkdir('photos')
 logger.add('../log/info.log', format='{time} {level} {message}', rotation='200kB', compression='zip')
 logger.info('Bot загрузился')
