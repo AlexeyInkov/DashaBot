61f05596276e16b6675ce887798f0a27f8b489ea
Alexey Inkov <alex0982@mail.ru>
2024-02-10 16:39:55 +0300

    refactor code

--- DashBot/__main__.py
+++ DashBot/__main__.py
@@ -5,7 +5,7 @@ from database.model import db, close_db
 from loader import bot
 import handlers
 from telebot.custom_filters import StateFilter
-# from no_close import keep_alive
+
 
 from loguru import logger
 
@@ -14,9 +14,7 @@ if __name__ == "__main__":
     try:
         bot.add_custom_filter(StateFilter(bot))
         bot.set_my_commands([BotCommand(i, COMMANDS[i][0]) for i in COMMANDS])
-        
-        # keep_alive()  # обманка для Replit
-        
+
         bot.polling(none_stop=True)
 
     except Exception:
